f0f59b1230980255210f4960d2b61b2f4ffd52d5
tot121212 <totmusicbusiness@gmail.com>
2025-03-18 14:55:44 -0500

    babel + jest + webpack integration

--- webpack.common.js
+++ webpack.common.js
@@ -1,37 +1,36 @@
-// webpack.config.js
-const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
+export default {
   entry: {
-    app: "./src/index.js",
+    app: './src/index.js',
   },
-  
+
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(path.dirname('./src'), 'dist'),
     clean: true,
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-        template: "./src/template.html",
+      template: './src/template.html',
     }),
   ],
-  
+
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
