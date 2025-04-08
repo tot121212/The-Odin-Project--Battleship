7080710d4b68c096ac4f31084e6f6272b1faf9f1
tot121212 <totmusicbusiness@gmail.com>
2025-04-08 13:32:35 -0500

    add enums for session and user actions

---
+++ shared/enums.mjs
@@ -0,0 +1,13 @@
+export const SESSION_ACTIONS = {
+    INIT_GAME: 'initGame',
+    CONNECT: 'connect',
+    DISCONNECT: 'disconnect',
+    START_GAME: 'startGame',
+};
+
+export const USER_ACTIONS = {
+    GET_SESSION_ID: 'getSessionID',
+    JOIN_GAME: 'joinGame',
+    LEAVE_GAME: 'leaveGame',
+    START_GAME: 'startGame',
+};
